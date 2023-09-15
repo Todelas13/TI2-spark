f2ef88d75508c1343e7080b2a0b4627de1f5c653
Todelas13 <120015178+Todelas13@users.noreply.github.com>
2023-09-15 04:30:12 -0300

    ++

--- spark/src/main/java/dao/DAO.java
+++ spark/src/main/java/dao/DAO.java
@@ -18,7 +18,7 @@ public class DAO {
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "postgres";
-		String password = "galo1302";
+		String password = "";
 		boolean status = false;
 
 		try {
